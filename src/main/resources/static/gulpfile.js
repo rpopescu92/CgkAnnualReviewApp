fa337eed3303b54a2275643f0615500ee69bfc4e
rpopescu <popescu.roxana.andreea@gmail.com>
2016-09-11 23:21:24 +0300

    gulp (almost) setup

--- src/main/resources/static/gulpfile.js
+++ src/main/resources/static/gulpfile.js
@@ -3,30 +3,11 @@ var gulp = require('gulp'),
     bowerFiles = require('main-bower-files'),
     angularFileSort = require('gulp-angular-filesort');
 
-gulp.task('inject:app', function () {
+gulp.task('inject', function () {
   var target = gulp.src('webapp/index.html');
-  var customSources = gulp.src('webapp/app/**/*.js', {
-                starttag: '<!-- inject:custom:{{ext}} -->',
-                transform: function (filePath, file) {
-                      // return file contents as string
-                      return file.contents.toString('utf8')
-                    }
-            })
-            .pipe(angularFileSort());
+  var sources = gulp.src(bowerFiles(), {read: false});
 
-  return target
-            .pipe(inject(customSources))
-            .pipe(gulp.dest('webapp'));
+  return target.pipe(inject(sources))
+            .pipe(inject(gulp.src('app/**/*.js')))
+    .pipe(gulp.dest('webapp'));
 });
-
-gulp.task('inject:vendor', function(){
-    var target = gulp.src('webapp/index.html');
-     var sources = gulp.src(bowerFiles(), {read: false})
-                    .pipe(inject(gulp.src('app/**/*.js')));
-
-
-   return target
-                .pipe(inject(sources))
-                .pipe(gulp.dest('webapp'));
-});
-
