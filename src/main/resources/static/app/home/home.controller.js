daa2dda5e2fdc8b72bacf640e05c872b03c648a6
rpopescu92 <popescu.roxana.andreea@gmail.com>
2016-10-28 16:48:43 +0200

    some karma

--- src/main/resources/static/app/home/home.controller.js
+++ src/main/resources/static/app/home/home.controller.js
@@ -5,13 +5,14 @@
         .module('bankApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', '$state', 'AuthorizationService','Account'];
+    HomeController.$inject = ['$scope', '$state', 'AuthorizationService','Account', 'Principal'];
 
-    function HomeController($scope, $state, AuthorizationService, Account) {
+    function HomeController($scope, $state, AuthorizationService, Account, Principal) {
         $scope.message = 'Home Controller';
 
         $scope.logout = logout;
         $scope.currentUser = currentUser;
+        $scope.account = null;
 
         init();
 
@@ -28,6 +29,12 @@
         function currentUser(){
             return Account.getAccount();
         }
+         function getAccount() {
+                    Principal.identity().then(function (account) {
+                        vm.account = account;
+                        vm.isAuthenticated = Principal.isAuthenticated;
+                    });
+                }
 
     }
 
--- src/main/resources/static/app/login/login.controller.js
+++ src/main/resources/static/app/login/login.controller.js
@@ -44,6 +44,7 @@
         }
 
         function register() {
+        console.log("register");
             $state.go('register');
         }
 
