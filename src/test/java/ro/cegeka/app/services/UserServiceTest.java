6a473d28bd3655998fafac04b28e3ea68855b856
Popescu Roxana <roxana.popescu@cegeka.com>
2016-11-03 11:05:41 +0200

    tests start

---
+++ src/test/java/ro/cegeka/app/services/UserServiceTest.java
@@ -0,0 +1,16 @@
+package ro.cegeka.app.services;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ro.cegeka.app.CegekaApplication;
+
+/**
+ * Created by roxanap on 03.11.2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {CegekaApplication.class})
+public class UserServiceTest {
+
+
+}
